8e86fddd122d1ed703d3a4e689f11bcf2a496e2e
youtheyeon <yuseoyeon@ewhain.net>
2026-04-30 13:16:19 +0900

    refactor: CreatePostResponse record 전환

--- src/main/java/org/sopt/dto/response/CreatePostResponse.java
+++ src/main/java/org/sopt/dto/response/CreatePostResponse.java
@@ -1,9 +1,6 @@
 package org.sopt.dto.response;
 
-public class CreatePostResponse {
-    public Long id;
-
-    public CreatePostResponse(Long id) {
-        this.id = id;
-    }
+public record CreatePostResponse(
+        Long id
+) {
 }
