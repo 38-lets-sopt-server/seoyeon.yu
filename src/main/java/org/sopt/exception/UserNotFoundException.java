201246f91bfecafb362866e20649459764dc5d1d
youtheyeon <yuseoyeon@ewhain.net>
2026-04-30 13:03:34 +0900

    fix: UserNotFoundException 에러 코드 수정

--- src/main/java/org/sopt/exception/UserNotFoundException.java
+++ src/main/java/org/sopt/exception/UserNotFoundException.java
@@ -2,10 +2,10 @@ package org.sopt.exception;
 
 public class UserNotFoundException extends BaseException {
     public UserNotFoundException() {
-        super(ErrorCode.POST_NOT_FOUND);
+        super(ErrorCode.USER_NOT_FOUND);
     }
 
     public UserNotFoundException(Long id) {
-        super(ErrorCode.POST_NOT_FOUND);
+        super(ErrorCode.USER_NOT_FOUND);
     }
 }
