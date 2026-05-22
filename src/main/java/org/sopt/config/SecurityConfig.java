e9c22e4baa822adc180a51f1c8bccc5b46942f60
youtheyeon <yuseoyeon@ewhain.net>
2026-05-22 23:48:59 +0900

    fix: post 조회 API permitAll() 설정

--- src/main/java/org/sopt/config/SecurityConfig.java
+++ src/main/java/org/sopt/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.sopt.security.JwtAuthFilter;
 import org.sopt.security.SecurityErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -33,6 +34,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/signup", "/api/v1/auth/login", "/api/v1/auth/reissue").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/posts", "/api/v1/posts/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex -> ex
